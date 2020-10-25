2bbc3ec4d9b5ca552a3227d6e77735a66bd0d5b7
jeovazero <jeovapereira2000@live.com>
2020-10-25 03:22:57 -0300

    chore: update docker config

--- docker.nix
+++ docker.nix
@@ -11,8 +11,19 @@ pkgs.dockerTools.buildImage {
   name = "jeovazero/pepe-haskeller";
   contents = [ pkgs.cacert resources ];
   created = "now";
+  runAsRoot = ''
+    #!${pkgs.runtimeShell}
+    mkdir -p /output
+  '';
   config = {
     Cmd = [ "${project}/bin/bot-poc" ];
+    Env = [
+      "BOT_RESOURCE_DIR=/data" # from resources.nix
+      "BOT_OUTPUT_DIR=/output"
+    ];
+    Volumes = {
+      "/output" = {};
+    };
   };
 }
 
